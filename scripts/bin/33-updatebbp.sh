4080e51bcedb791a8e9a2d62d58e3be6017aad35
bull <bullbull@gmail.com>
2024-08-30 11:56:58 +0800

    update

--- scripts/bin/33-updatebbp.sh
+++ scripts/bin/33-updatebbp.sh
@@ -4,8 +4,8 @@ while read line; do
     bbp_array+=("$line")
 done < ./33-bbpupdate_list.txt
 
-# mcli='amcli -u https://expnode.amaxscan.io'
-mcli="amcli -u http://sh-amnod.vmi.amax.dev:18188" 
+mcli='amcli -u https://expnode.amaxscan.io'
+# mcli="amcli -u http://sh-amnod.vmi.amax.dev:18188" 
 mpkey=AM898h1RX9ycXSiEwu4CF4Ame5HXxtsjqsYqAm6kTwvi7ZBaHuQs
 for i in "${!bbp_array[@]}"; do
     bbp=${bbp_array[$i]}
