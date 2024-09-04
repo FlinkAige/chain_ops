823739b41517ab7a4427dba1df1761893d161647
bull <bullbull@gmail.com>
2024-09-04 11:50:38 +0800

    update

--- scripts/bin/40-setbbpcliam.sh
+++ scripts/bin/40-setbbpcliam.sh
@@ -9,7 +9,7 @@ function addbbps(){
         # 在这里对每一行的内容做操作
         parms=$line
         rewarder=$(echo $parms | cut -d '-' -f1)
-        bbps=$(echo $parms | cut -d '-' -f1)
+        bbps=$(echo $parms | cut -d '-' -f2)
         $mcli push action $con_bbp addbbp '[ ['$bbps'],"'$rewarder'"]' -p $con_bbp
     done
 }
