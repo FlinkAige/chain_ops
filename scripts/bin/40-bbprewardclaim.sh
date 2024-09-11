23f8cc41b72289d3d714e0d04e38cedb8d43f66b
bull <bullbull@gmail.com>
2024-09-11 13:35:29 +0800

    update

--- scripts/bin/40-bbprewardclaim.sh
+++ scripts/bin/40-bbprewardclaim.sh
@@ -14,8 +14,8 @@ function bbpclaimreward(){
 
         echo "claim: $i" && $mcli push action $con_bbp claimbbps '[50]' -p $submiter 2>&1 | \
              echo "done claiming" && exit 1
-
-  i=$((i + 1))
+    
+        i=$((i + 1))
     done
 
 }
