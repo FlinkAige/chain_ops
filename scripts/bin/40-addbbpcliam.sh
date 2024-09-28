843ac45049cd30bbe94a3e048c71544eeeb5c666
bull <bullbull@gmail.com>
2024-09-28 15:43:58 +0800

    add shell script

--- scripts/bin/40-addbbpcliam.sh
+++ scripts/bin/40-addbbpcliam.sh
@@ -4,14 +4,14 @@
 
 mcli='amcli -u https://expnode.amaxscan.io'
 con_bbp=amaxapplybbp
-filename="40-ibbp-split-240928.txt"
-function setbbpreward(){
+filename="40-ibbp-split.txt"
+function addbbpsward(){
     cat $filename | while IFS= read -r line; do
         # 在这里对每一行的内容做操作
         parms=$line
         rewarder=$(echo $parms | cut -d '-' -f1)
         bbps=$(echo $parms | cut -d '-' -f2)
-        $mcli push action $con_bbp setbbpreward '[ ['$bbps'],"'$rewarder'"]' -p $con_bbp
+        $mcli push action $con_bbp addbbpreward '[ ['$bbps'],"'$rewarder'"]' -p $con_bbp
     done
 }
-setbbpreward
+addbbpsward
