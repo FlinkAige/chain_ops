2eea16af4da617d79139a524c3e7c68fd1963532
Joss <281900360@qq.com>
2024-12-27 09:19:46 +0800

    update

--- scripts/bin/2.vote/1-gen_bbp_account_30.py
+++ scripts/bin/2.vote/1-gen_bbp_account_30.py
@@ -6,23 +6,36 @@ def save_string_to_file(string, filename):
     with open(filename, 'a') as file:
         file.write(string)
 
+def add_vote_command(account):
+    command = "$mpush amax addvote '[\"" + account + "\", \"1000.0000 VOTE\"]' -p" + account
+    save_string_to_file(command + "\n", "./internal_add_vote.sh")
+
 def gen_vote_command(account, voters):
     command = "$mpush amax vote '[\"" + account + "\", [" + voters + "]]' -p" + account
-    save_string_to_file(command + "\n", "./12-bbp_vote_34.sh")
+    save_string_to_file(command + "\n", "./internal_vote.sh")
+   
 def gen_ovotes_group():
     i=0
     voters=""
     accounts=[]
-    for i1 in range(0,2):
-        for i2 in range(0,10):
+    for i1 in range(1,2):
+        for i2 in range(0,5):
             for i3 in range(0,10):
                 for i4 in range(0,10):
-                        acc=list[i1]+list[i2]+list[i3]+list[i4] +"v.bbp"
-                        voters+= "\"" + acc  + "\"" + ","
+                        acc=list[i1]+list[i2]+list[i3]+list[i4] +".bbp"
+                        voters+= "\"" + acc  + "\""
                         accounts.append(acc)
                         i+=1
-                        if(i%50==0):
-                            save_string_to_file(voters + "\n", "./18-obbp-voter-list.txt")
+                        if(i%30==0):
+                            index = int(i/30)
+                            add_vote_command(accounts[index-1])
+                            gen_vote_command(accounts[index-1], voters)
                             voters=""
+                        else:
+                            voters+=","
+    if(i%30!=0):
+        index = int(i/30)
+        add_vote_command(accounts[index])
+        gen_vote_command(accounts[index], voters)
                             
 gen_ovotes_group()
---
+++ scripts/bin/2.vote/internal_add_vote.sh
@@ -0,0 +1,18 @@
+mpush='amcli -u https://expnode.amaxscan.io push action'
+$mpush amax addvote '["baaa.bbp", "1000.0000 VOTE"]' -pbaaa.bbp
+$mpush amax addvote '["baab.bbp", "1000.0000 VOTE"]' -pbaab.bbp
+$mpush amax addvote '["baac.bbp", "1000.0000 VOTE"]' -pbaac.bbp
+$mpush amax addvote '["baad.bbp", "1000.0000 VOTE"]' -pbaad.bbp
+$mpush amax addvote '["baae.bbp", "1000.0000 VOTE"]' -pbaae.bbp
+$mpush amax addvote '["baaf.bbp", "1000.0000 VOTE"]' -pbaaf.bbp
+$mpush amax addvote '["baag.bbp", "1000.0000 VOTE"]' -pbaag.bbp
+$mpush amax addvote '["baah.bbp", "1000.0000 VOTE"]' -pbaah.bbp
+$mpush amax addvote '["baai.bbp", "1000.0000 VOTE"]' -pbaai.bbp
+$mpush amax addvote '["baaj.bbp", "1000.0000 VOTE"]' -pbaaj.bbp
+$mpush amax addvote '["baba.bbp", "1000.0000 VOTE"]' -pbaba.bbp
+$mpush amax addvote '["babb.bbp", "1000.0000 VOTE"]' -pbabb.bbp
+$mpush amax addvote '["babc.bbp", "1000.0000 VOTE"]' -pbabc.bbp
+$mpush amax addvote '["babd.bbp", "1000.0000 VOTE"]' -pbabd.bbp
+$mpush amax addvote '["babe.bbp", "1000.0000 VOTE"]' -pbabe.bbp
+$mpush amax addvote '["babf.bbp", "1000.0000 VOTE"]' -pbabf.bbp
+$mpush amax addvote '["babg.bbp", "1000.0000 VOTE"]' -pbabg.bbp
